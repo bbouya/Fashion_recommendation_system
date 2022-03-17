dea5266b4c68bee6dca0195c635b883b92280470
bbouya <52068209+bbouya@users.noreply.github.com>
2022-03-17 16:35:09 +0100

    Update fashion_input.py

--- code/fashion_input.py
+++ code/fashion_input.py
@@ -12,3 +12,22 @@ from hyper_parameters import *
 shuffle = True  
 localization = FLAGS.is_localization
 imageNet_mean_pixel = [103.939,116.799,123.68]
+global_std = 68.76 
+
+IMG_ROWS = 64
+IMG_COLS = 64
+
+def get_image(path, x1, y1, x2,y2):
+    '''
+    :param path : image path
+    :param x1: the upper left and lower 
+    :param y1 : the upper left and
+    :param x2 et y2.
+    : return : a numpy array with dimension [img_row, img_col, img_depth]
+    '''
+
+    img = cv2.imread(path)
+    if localization is True:
+        if img is None or img.shape[0] == 0 or img.shape[1] == 0:
+            img = np.zeros((1,IMG_ROWS,IMG_COLS))
+    img = cv2
