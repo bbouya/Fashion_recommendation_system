072f68196b7be4591b03b72f8a27475b71261789
bbouya <52068209+bbouya@users.noreply.github.com>
2022-03-17 16:28:56 +0100

    Update fashion_input.py

--- code/fashion_input.py
+++ code/fashion_input.py
@@ -1,3 +1,14 @@
 '''
 This python file is responsible for the image processing.
 '''
+
+from operator import ilshift, ipow
+from sre_parse import FLAGS
+import cv2
+import numpy as np
+import pandas as pd
+from hyper_parameters import *
+
+shuffle = True  
+localization = FLAGS.is_localization
+imageNet_mean_pixel = [103.939,116.799,123.68]
