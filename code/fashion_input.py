23b8c777038a3914689b76a85259cff44c3044de
bbouya <52068209+bbouya@users.noreply.github.com>
2022-03-17 16:37:44 +0100

    Update fashion_input.py

--- code/fashion_input.py
+++ code/fashion_input.py
@@ -3,6 +3,7 @@ This python file is responsible for the image processing.
 '''
 
 from operator import ilshift, ipow
+from sre_constants import ASSERT
 from sre_parse import FLAGS
 import cv2
 import numpy as np
@@ -30,4 +31,11 @@ def get_image(path, x1, y1, x2,y2):
     if localization is True:
         if img is None or img.shape[0] == 0 or img.shape[1] == 0:
             img = np.zeros((1,IMG_ROWS,IMG_COLS))
-    img = cv2
+        img = cv2.resize(img,IMG_ROWS,IMG_COLS)
+        assert img.shape == (IMG_ROWS,IMG_COLS,3)
+    else:
+        img = cv2.resize(img,IMG_ROWS,IMG_COLS)
+    
+    img = img.reshape(1, IMG_ROWS,IMG_COLS,3)
+
+    return img
